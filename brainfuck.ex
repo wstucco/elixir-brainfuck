ef917a87d454c346e4504e8103431c2e2bb55f34
Massimo Ronca <massimo@mikamai.com>
2014-10-18 00:10:59 +0200

    remove needless pattern matching

--- brainfuck.ex
+++ brainfuck.ex
@@ -3,19 +3,9 @@
 defmodule BrainfuckCli do
 	# import Brainfuck
 
-  def run(args) do
-    {_addr, _mem, out} = Brainfuck.run args |> parse_args
+  def run(argv) do
+    {_addr, _mem, out} = Brainfuck.run File.read! "#{argv}"
     IO.write out
-  end
-
-  defp parse_args([]), do: raise "missing file parameter"
-  defp parse_args([file | _]), do: parse_args file
-
-  defp parse_args(file) do
-    case File.read "#{file}" do
-      {:ok, body}   -> body
-      {:error, err} -> raise "error loading file #{file}: #{err}"
-    end
   end
 
 end
