dc474c6b5c97cebf557804ea39d5b71fc2c7eaec
Massimo Ronca <massimo@mikamai.com>
2014-10-18 00:13:06 +0200

    remove needles import

--- brainfuck.ex
+++ brainfuck.ex
@@ -1,13 +1,10 @@
 #!/usr/bin/env elixir
 
 defmodule BrainfuckCli do
-	# import Brainfuck
-
   def run(argv) do
     {_addr, _mem, out} = Brainfuck.run File.read! "#{argv}"
     IO.write out
   end
-
 end
 
 
