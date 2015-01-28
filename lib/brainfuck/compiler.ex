9f2a323431ee409e12cc1345c0ae6e2f2ec14ee9
Massimo Ronca <massimo@mikamai.com>
2015-01-28 21:06:10 +0100

    match char code points directly, instead of converting them to string

--- lib/brainfuck/compiler.ex
+++ lib/brainfuck/compiler.ex
@@ -10,16 +10,16 @@ defmodule Brainfuck.Compiler do
   defp compile([], ast, stack) when length(stack) == 0, do: ast
 
   defp compile([ic | tail], ast, stack) do
-    case {[ic] |> to_string, stack} do
-      { "+", _ } -> compile tail, ast ++ [:inc_d], stack
-      { "-", _ } -> compile tail, ast ++ [:dec_d], stack
-      { ">", _ } -> compile tail, ast ++ [:inc_p], stack
-      { "<", _ } -> compile tail, ast ++ [:dec_p], stack
-      { ".", _ } -> compile tail, ast ++ [:put_c], stack
-      { ",", _ } -> compile tail, ast ++ [:get_c], stack
-      { "[", _ } -> compile tail, [], [ast] ++ stack
-      { "]", [] } -> raise "unmatched ']'"
-      { "]", [h | t] } -> compile tail, h ++ [loop: ast], t
+    case {[ic], stack} do
+      { '+', _ } -> compile tail, ast ++ [:inc_d], stack
+      { '-', _ } -> compile tail, ast ++ [:dec_d], stack
+      { '>', _ } -> compile tail, ast ++ [:inc_p], stack
+      { '<', _ } -> compile tail, ast ++ [:dec_p], stack
+      { '.', _ } -> compile tail, ast ++ [:put_c], stack
+      { ',', _ } -> compile tail, ast ++ [:get_c], stack
+      { '[', _ } -> compile tail, [], [ast] ++ stack
+      { ']', [] } -> raise "unmatched ']'"
+      { ']', [h | t] } -> compile tail, h ++ [loop: ast], t
       _ -> compile tail, ast, stack
     end
   end
